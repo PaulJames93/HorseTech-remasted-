d354bf42352fa233a7d8adf0d910d8688fa21b50
Pavel Chirkov <ienglishman187@gmail.com>
2021-01-09 19:45:27 +0300

    Initial Commit

---
+++ HorseTech(remasted)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HorseTech(remasted)
+//
+//  Created by Paul James on 09.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
